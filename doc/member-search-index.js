8bc25195d83dd688313ed4d94f0851b697130ea0
KaterynaKovalKI307 <145603460+KaterynaKovalKI307@users.noreply.github.com>
2023-09-22 18:58:34 +0300

    Add files via upload

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Lab1KovalKI307","l":"Lab1KovalKI307()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab1KovalKI307","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
